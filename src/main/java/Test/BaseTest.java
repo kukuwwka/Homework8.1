5cad4d65602ee0618b89a269b6080ada2d5f130d
kukuwwka <ququwka1@gmail.com>
2023-11-23 21:49:41 +0400

    initial commit

---
+++ src/main/java/Test/BaseTest.java
@@ -0,0 +1,99 @@
+package Test;
+
+import Main.MainPage;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.Keys;
+
+@Slf4j
+public class BaseTest {
+    WebDriver webDriver;
+    MainPage mainPage;
+
+    @Test
+    public void checkboxes() throws InterruptedException {
+        webDriver = new ChromeDriver();
+        webDriver.get("http://the-internet.herokuapp.com/checkboxes");
+
+        WebElement checkInput=webDriver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]"));
+        checkInput.click();
+    }
+    @Test
+    public void positiveFormAuthentication() throws InterruptedException {
+        webDriver = new ChromeDriver();
+        webDriver.get("http://the-internet.herokuapp.com/login");
+
+        WebElement login = webDriver.findElement(By.xpath("//*[@id=\"username\"]"));
+        WebElement password = webDriver.findElement(By.xpath("//*[@id=\"password\"]"));
+
+        login.sendKeys("tomsmith");
+        password.sendKeys("SuperSecretPassword!");
+
+        WebElement button = webDriver.findElement(By.xpath("//*[@id=\"login\"]/button"));
+        button.click();
+    }
+
+    @Test
+    public void negativeFormAuthentication() throws InterruptedException {
+        webDriver = new ChromeDriver();
+        webDriver.get("http://the-internet.herokuapp.com/login");
+
+        WebElement login = webDriver.findElement(By.xpath("//*[@id=\"username\"]"));
+        WebElement password = webDriver.findElement(By.xpath("//*[@id=\"password\"]"));
+
+        login.sendKeys("tf3weg");
+        password.sendKeys("1221!");
+
+        WebElement button = webDriver.findElement(By.xpath("//*[@id=\"login\"]/button"));
+        button.click();
+    }
+
+    @Test
+    public void hovers() throws InterruptedException {
+        webDriver = new ChromeDriver();
+        webDriver.get("http://the-internet.herokuapp.com/hovers");
+
+        WebElement clickable = webDriver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/img"));
+        new Actions(webDriver)
+                .moveToElement(clickable)
+                .perform();
+    }
+
+    @Test
+    public void dynamicallyLoadedPageElements() throws InterruptedException {
+        webDriver = new ChromeDriver();
+        webDriver.get("http://the-internet.herokuapp.com/dynamic_loading/1");
+
+        WebElement button = webDriver.findElement(By.xpath("//*[@id=\"start\"]/button"));
+        button.click();
+
+        Thread.sleep(5000);
+
+        String feedMessage = webDriver.findElement(By.xpath("//*[@id=\"finish\"]/h4")).getText();
+        System.out.println("Дождались: " + feedMessage);
+    }
+
+    @Test
+    public void keyPresses() throws InterruptedException {
+        webDriver = new ChromeDriver();
+        webDriver.get("http://the-internet.herokuapp.com/key_presses");
+
+        WebElement enterText = webDriver.findElement(By.xpath("//*[@id=\"target\"]"));
+
+        WebElement button = webDriver.findElement(By.xpath("//*[@id=\"target\"]"));
+        button.click();
+
+        new Actions(webDriver)
+                .sendKeys("e")
+                .perform();
+    }
+}
